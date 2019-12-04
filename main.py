798088fbe53f588ac69bd82438b20e85fde0ecc4
suhaylx <akmsodikov@gmail.com>
2019-12-04 14:12:10 +0000

    using for and while together , while true situation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 i = 0 
 while i <= 50:
   print(i)
@@ -12,3 +13,29 @@ q = 0
 while q < 50:
   print(q)
   break
+
+
+
+
+#################################################################################
+#using while and for 
+
+my_list = list(range(1,100))
+item = 0
+
+while item < len(my_list):
+  print(my_list[item])
+  item +=1
+
+
+for items in my_list:
+  print(items)
+
+
+###while true situations that asking for some action or process
+
+while True:
+  response = input("Say something : ")
+  if(response == 'byee'):
+    print ("Okay byee")
+    break
